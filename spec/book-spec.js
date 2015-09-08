d7f181deceeb1f6326fda162cd305eb2ad60d3d5
scq000 <scq000@hotmail.com>
2015-09-08 15:06:02 +0800

    change the test of book

--- spec/book-spec.js
+++ spec/book-spec.js
@@ -5,12 +5,12 @@ var Book = require('../src/book');
 describe('Book', function () {
 
     it('should have a title', function () {
-        var book = new Book('First');
-        expect(book.title).toBe('First');
+        var book = new Book('A');
+        expect(book.title).toBe('A');
     });
 
     it('should have a default price that is 8', function () {
-        var book = new Book('First');
+        var book = new Book('A');
         expect(book.price).toBe(8);
     });
 });
